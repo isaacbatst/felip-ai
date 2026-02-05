27e50b05274f2e1a369aeb686cc5c0ec5e4f66bd
Isaac Batista <isaacbatst@gmail.com>
2026-02-05 15:53:47 -0300

    refactor: clean up whitespace in worker manager swarm configuration
    - Removed unnecessary whitespace in the worker manager swarm configuration file, improving code readability and consistency.
    - This change does not affect functionality but enhances the overall code quality.

--- nest_api/src/infrastructure/workers/worker-manager-swarm.ts
+++ nest_api/src/infrastructure/workers/worker-manager-swarm.ts
@@ -179,7 +179,7 @@ export class WorkerManagerSwarm extends WorkerManager implements OnModuleDestroy
           },
           Placement: {
             Constraints: [],
-          },
+          },  
         },
         Mode: {
           Replicated: {
